d671b5da458946197c81e66f40c21d893d39896a
Gusrot <gnrotela@yahoo.com.ar>
2017-11-06 18:01:02 -0300

    lectura archivos

--- src/pages/archivos/archivos.ts
+++ src/pages/archivos/archivos.ts
@@ -10,58 +10,100 @@ import 'rxjs/add/operator/map';
   templateUrl: 'archivos.html',
 })
 export class ArchivosPage {
+  public csvItem: any[] = [];
   csvData: any[] = [];
   headerRow: any[] = [];
+  col1 : any[][] = [];
+  col2 : any[][] = [];
 
   constructor(public navCtrl: NavController, private http: Http) {
     this.readCsvData();
   }
 
-  private readCsvData() {
-    this.http.get('assets/PPS -4A-2c2017.csv')
-      .subscribe(
-      data => this.extractData(data),
-      err => this.handleError(err)
-      );
+  formatParsedObject(arr, hasTitles) {
+    //console.log("array",arr);
+    let legajo,
+      nom,
+      cursa,
+      obj = [];
+
+    for (var j = 0; j < arr.length-1; j++) {
+      var items = arr[j][0];
+      var items1 = arr[j][1];
+      let array = items.split(";");
+      let array1 = items1.split(";");
+       /*if (items.indexOf("") === -1) {
+        obj.push({
+          legajo: items[0],
+          nom: items[1]
+        });
+      }*/
+      /*console.log(arr[j][0].indexOf(";"));
+      console.log(arr[j][1].indexOf(";"));*/
+      
+      obj.push({
+        legajo: array[0],
+        ape: array[1],
+        nom: array1[0],
+        dia: array1[1]
+      });
+    }
+    this.csvItem = obj;
+    console.log(this.csvItem);
   }
 
+
+  //#region Gustavo
+  
+  
+  
+  
+  private readCsvData() {
+    this.http.get('assets/PPS -4A-2c2017.csv')
+    .subscribe(
+    data => this.extractData(data),
+    err => this.handleError(err)
+    );
+  }
+  
   private extractData(res) {
-    let csvData = res['_body'] || '';
-    let parsedData = baby.parse(csvData, {
+    let csvData = res['_body'] || '' ;
+    let parsedData = baby.parse(csvData,{
       header: false,
       dynamicTyping: true,
       encoding: "UTF-8"
     }).data;
-
-    /*this.headerRow = parsedData[0];
- 
-    parsedData.splice(0, 1);*/
+  
     this.csvData = parsedData;
-  }
 
-  downloadCSV() {
+    console.log("despues CSV",this.formatParsedObject(this.csvData,false));
+  }
+  
+  downloadCSV() 
+  {
     let csv = baby.unparse({
       fields: this.headerRow,
       data: this.csvData
     });
+  
+    // Dummy implementation for Desktop download purpose
     var blob = new Blob([csv]);
-    var a = document.createElement("a");
-    a.href = 'assets/PPS -4A-2c2017.csv';
+    var a = window.document.createElement("a");
+    a.href = window.URL.createObjectURL(blob);
     a.download = "newdata.csv";
-    if(document.body.appendChild(a))
-    {
-      alert('se descargo correctamente');
-    }
+    document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
   }
-
+  
   private handleError(err) {
     console.log('something went wrong: ', err);
   }
-
+  
   trackByFn(index: any, item: any) {
     return index;
   }
+  
+  //#endregion
 
 }
