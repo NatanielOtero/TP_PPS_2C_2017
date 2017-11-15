d28a537019ede0d0ea54f77b4bc675a99e042a21
NatanielOtero <natanielotero@gmail.com>
2017-11-15 15:10:22 -0300

    cambios de asistencia y eso

--- src/pages/asistencia/asistencia.ts
+++ src/pages/asistencia/asistencia.ts
@@ -1,3 +1,4 @@
+import { Asistencia } from '../../entidades/asist';
 import { Alumno } from '../../entidades/alumnos';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -22,26 +23,58 @@ export class AsistenciaPage {
   logo: any;
   opcion : string;
   fecha : string;
+  mostrar : boolean = true;
   public Items: AngularFireList<any>;
   public items: Observable<any>;
+  asist : Array<any> = new Array<any>();
   
+ 
   constructor(public navCtrl: NavController, private http: Http, public afDB: AngularFireDatabase) {
     
     this.fecha = new Date().toLocaleDateString().toString();
+    console.log(this.fecha);
   }
 
-  async leerDB() {
+   leerDB() {
     this.Items = this.afDB.list('/' + this.opcion + "/");
     this.items = this.Items.valueChanges();
     this.items.subscribe(cantidad => this.cant = cantidad);
   }
-
+  c()
+  {
+    console.log(this.asist);
+  }
   f()
   {
-    this.leerDB();
+    this.crearLista();
+    if(this.cant != null)
+    {
+      this.mostrar = false;
+    }
+    else
+    {
+      alert("carga la base papu");
+    }
     console.log(this.cant);
   }
+    crearLista()
+    {
+      this.leerDB();
+      for (var i = 0; i < this.cant.length; i++) {
+            
+        this.asist[i] = this.cant[i];
+       this.asist[i].vino = false;
+     
+        
+      }
+      console.log(this.asist);
+    }
 
+    guardar()
+    {
+      const itemsRef = this.afDB.list('/' + this.opcion + '/' + this.fecha + '/');
+      itemsRef.push(this.asist);
+    }
   ionViewDidLoad() {
     console.log('ionViewDidLoad AsistenciaPage');
   }
