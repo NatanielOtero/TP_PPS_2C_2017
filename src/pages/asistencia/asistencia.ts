7ee26d2629e265194670207cdb436b6c8be5efca
NatanielOtero <natanielotero@gmail.com>
2017-11-16 22:05:26 -0300

    pero mira como toma lista papaa

--- src/pages/asistencia/asistencia.ts
+++ src/pages/asistencia/asistencia.ts
@@ -1,3 +1,4 @@
+import { isEmpty } from 'rxjs/operator/isEmpty';
 import { Asistencia } from '../../entidades/asist';
 import { Alumno } from '../../entidades/alumnos';
 import { Component, OnInit } from '@angular/core';
@@ -21,60 +22,75 @@ import { Http } from '@angular/http';
 export class AsistenciaPage {
   public cant: Array<any> = new Array<any>();
   logo: any;
-  opcion : string;
-  fecha : string;
-  mostrar : boolean = true;
+  opcion: string;
+  opcion1: string;
+  fecha: any;
+  mostrar: boolean = true;
+  mostrar1: boolean = true;
   public Items: AngularFireList<any>;
   public items: Observable<any>;
-  asist : Array<any> = new Array<any>();
-  
- 
+  asist: Array<any> = new Array<any>();
+
   constructor(public navCtrl: NavController, private http: Http, public afDB: AngularFireDatabase) {
-    
+    //this.leer();
     this.fecha = new Date().toLocaleDateString().toString();
+    this.fecha = this.fecha.split('/');
     console.log(this.fecha);
   }
 
-   leerDB() {
-    this.Items = this.afDB.list('/' + this.opcion + "/");
+
+  async leerDB() {
+
+    this.Items = this.afDB.list('/lista/' + this.opcion + '-' + this.fecha[0] + '-' + this.fecha[1] + '-' + this.fecha[2] + '/' + this.opcion1);
+    this.items = this.Items.valueChanges();
+    this.items.subscribe(cantidad => this.cant = cantidad);  
+    setTimeout(() => {
+     if(this.cant.length==0)
+     {
+       this.leer();
+     }
+    }, 500);   
+
+  }
+
+  leer() {
+    this.Items = this.afDB.list('/' + this.opcion + "/" + this.opcion1 + "/");
     this.items = this.Items.valueChanges();
     this.items.subscribe(cantidad => this.cant = cantidad);
+    
   }
-  c()
-  {
+
+ 
+
+  f() {
+    this.leer(); 
+    this.mostrar = false;     
+  }
+  g() {
+    this.crearLista(); 
+   
+    this.mostrar1 = false;
+    
+  }
+
+  crearLista() {
+    //this.cant = new Array<any>();
+    this.leerDB();
+    for (var i = 0; i < this.cant.length; i++) {
+
+      //this.asist[i] = this.cant[i];
+      this.cant[i].vino = false;
+
+    }
+   
     console.log(this.asist);
   }
-  f()
-  {
-    this.crearLista();
-    if(this.cant != null)
-    {
-      this.mostrar = false;
-    }
-    else
-    {
-      alert("carga la base papu");
-    }
-    console.log(this.cant);
-  }
-    crearLista()
-    {
-      this.leerDB();
-      for (var i = 0; i < this.cant.length; i++) {
-            
-        this.asist[i] = this.cant[i];
-       this.asist[i].vino = false;
-     
-        
-      }
-      console.log(this.asist);
-    }
+ 
 
-    guardar()
-    {
-      const itemsRef = this.afDB.list('/' + this.opcion + '/' + this.fecha + '/');
-      itemsRef.push(this.asist);
-    }
+  guardar() {
+    const itemsRef = this.afDB.list('/lista/');
+    itemsRef.set(this.opcion + '-' + this.fecha[0] + '-' + this.fecha[1] + '-' + this.fecha[2] + '/' + this.opcion1, this.cant);
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad AsistenciaPage');
   }
